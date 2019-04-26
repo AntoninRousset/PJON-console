0374fd8670e8f4910fca989211afd1f3e9dd7a46
AntoninRousset <AntoninRousset@users.noreply.github.com>
2019-04-27 01:26:12 +0200

    working console with abstract socket. cannot properly echo in /dev/pts/x though

--- console.c
+++ console.c
@@ -26,7 +26,55 @@
 
 #include <sys/select.h>
 #include <sys/wait.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 
+#define SOCKET_FILE "/tmp/PJON.sock"
+#define MSG_LENGTH 2048
+
+int open_socket(const char *filename)
+{
+	int sock = socket(PF_LOCAL, SOCK_STREAM, 0);
+	if (sock < 0) {
+		perror ("socket");
+		exit (EXIT_FAILURE);
+	}
+
+	struct sockaddr_un name;
+	memset(&name, 0, sizeof(name));
+	name.sun_family = AF_LOCAL;
+	name.sun_path[0] = '\0';
+	strncpy(name.sun_path+1, filename, strlen(filename));
+	socklen_t size = offsetof(struct sockaddr_un, sun_path)
+				   + strlen(filename) + 1;
+	if (connect(sock, (struct sockaddr*) &name, size) < 0) {
+		perror("connect");
+		exit(EXIT_FAILURE);
+	}
+
+	return sock;
+}
+
+int write_socket(int sock, const char* data, size_t size)
+{
+	ssize_t count = send(sock, data, size, 0);
+	if (count < 0)
+		perror("Send");
+	return count;
+}
+
+// I am too stupid to create a function that take int* master, int* slave and
+// returns the name of the file
 int open_pty()
 {
 	int master = posix_openpt(O_RDWR);
@@ -35,32 +83,74 @@ int open_pty()
 		exit(EXIT_FAILURE);
 	}
 
+	if (grantpt(master) < 0) {
+		perror("grant");
+		exit(EXIT_FAILURE);
+	}
+
+	if (unlockpt(master) < 0) {
+		perror("unlock");
+		exit(EXIT_FAILURE);
+	}
 	return master;
+}
+
+// Can we write a single function 'write' for all fd?
+int write_pty(int fd, const char* msg, size_t size)
+{
+	ssize_t count = write(fd, msg, size);
+	if (count < 0)
+		perror("write pty");
+	return count;
 }
 
 int main()
 {
-	int master = open_pty();
-	printf("master number: %d\n", master);
+	int sock = open_socket(SOCKET_FILE);
 
-	fd_set set;
-	FD_ZERO(&set);
-	FD_SET(master, &set);
+	int master_pty = open_pty();
+	char* name_pty = ptsname(master_pty);
+	if (name_pty == NULL) {
+		perror("name pty");
+		close(master_pty);
+		exit(EXIT_FAILURE);
+	}
+	printf("PTY on %s\n", name_pty);
 
-	char buffer[2048];
+	int slave_pty = open(name_pty, O_WRONLY);
+	if (slave_pty < 0) {
+		perror("slave pty");
+		close(master_pty);
+		exit(EXIT_FAILURE);
+	}
 
-	for (;;) {
-		if (select(FD_SETSIZE, &set, NULL, NULL, NULL) < 0) {
-			perror("select");
-			exit(EXIT_FAILURE);
+	char buffer[MSG_LENGTH];
+	ssize_t count;
+
+	int pid = fork();
+	if (pid < 0) {
+		perror("fork");
+	}
+
+	if (pid == 0) {
+		while (1) {
+			count = read(master_pty, &buffer, sizeof(buffer));
+			if (count < 0) {
+				perror("Read pty");
+				return EXIT_FAILURE;
+			}
+			printf("read from pty: %s\n", buffer);
+			write_socket(sock, buffer, strlen(buffer)+1);
 		}
-
-		if (FD_ISSET(master, &set)) {
-			read(master, buffer, sizeof(buffer));
-			printf("%s", buffer);
+	} else {
+		while (1) {
+			fgets(buffer, MSG_LENGTH, stdin);
+			write_pty(slave_pty, buffer, strlen(buffer)+1);
 		}
 	}
+
+	shutdown(sock, 0);
+	shutdown(slave_pty, 0);
+	close(master_pty);
 	return EXIT_SUCCESS;
 }
-
-// We should mutli-thread it, one thread for in, one for out...
